c07b526c27f6bf343ec55d01dfb47a42395edb41
Alí Ansidey <55164581+alidav173@users.noreply.github.com>
2023-04-09 16:09:20 -0300

    Ejercicio 104 POO Boligrafo

--- Ejercicio_104 POO/Boligrafo.cs
+++ Ejercicio_104 POO/Boligrafo.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -9,15 +10,84 @@ namespace Ejercicio_104_POO
 {
   internal class Boligrafo
   {
-    private short cantidadMaximaTinta;
-    private ConsoleColor color;
-    private short tinta;
+    const short cantidadMaximaTinta = 100;
+    private ConsoleColor _color;
+    private short _tinta;
+
+
+    public ConsoleColor Color { get; set; }
+
+    public short Tinta
+    {
+      get => _tinta;
+      set => _tinta = value <= cantidadMaximaTinta ? value: cantidadMaximaTinta;
+    }
+
+
 
     public Boligrafo(short tinta, ConsoleColor color)
     {
-      this.tinta = tinta;
-      this.color = color;
+      this.Tinta = tinta;
+      this.Color = color;
     }
+
+    private void SetTinta(short tinta)
+    {
+ 
+      if(tinta >= 0)
+      {
+        Tinta = tinta;
+      }
+        
+    }
+
+    public void Recargar()
+    {
+      Tinta = cantidadMaximaTinta;
+    }
+
+    public bool Pintar(short gasto, out string dibujo)
+    {
+      bool bandera = false;
+      int diferencia = 0;
+      StringBuilder sb = new StringBuilder("",50);
+
+      if(Tinta > 0)
+      {
+        diferencia = Tinta - gasto;
+        if (diferencia > 0)
+        {
+          for (int i = 1; i < gasto + 1; i++)
+          {
+            sb.Append("*");
+          }
+          SetTinta((short)diferencia);
+
+        }
+
+        else if (diferencia <= 0)
+        {
+          for (int j = 1; j < Tinta + 1; j++)
+          {
+            sb.Append("*");
+          }
+     
+          SetTinta(0);
+        }
+        bandera = true;
+      }
+     
+      else if(Tinta == 0 | gasto == 0)
+      {
+        sb.Append("");
+      }
+      Console.ForegroundColor = Color;
+      dibujo = sb.ToString();
+
+      return bandera;
+    }
+
+
   }
 }
  
--- Ejercicio_104 POO/Program.cs
+++ Ejercicio_104 POO/Program.cs
@@ -9,7 +9,38 @@ namespace Ejercicio_104_POO
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello, World!");
+      string salida;
+      
+      Boligrafo boligrafoUno = new Boligrafo(100, ConsoleColor.Blue);
+      Boligrafo boligrafoDos = new Boligrafo(50, ConsoleColor.Red);
+
+
+      boligrafoUno.Pintar(100,out salida);
+      Console.WriteLine(salida);
+      
+      boligrafoDos.Pintar(50, out salida);
+      Console.WriteLine(salida);
+      
+      boligrafoUno.Pintar(5, out salida);
+      Console.WriteLine(salida);
+      
+      boligrafoDos.Pintar(10, out salida);
+      Console.Write(salida);
+
+      boligrafoUno.Recargar();
+
+      boligrafoUno.Pintar(50, out salida);
+      Console.WriteLine(salida);
+
+      boligrafoDos.Recargar();
+
+      boligrafoDos.Pintar(100, out salida);
+      Console.WriteLine(salida);
+
+      boligrafoUno.Pintar(50, out salida);
+      Console.WriteLine(salida);
+
+
     }
   }
 }
